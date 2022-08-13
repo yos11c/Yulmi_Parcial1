da0c4e3031047a03a5ec5bc8698393afe04f7b8c
yos11c <ycontrerasc3@miumg.edu.gt>
2022-08-13 09:44:43 +0000

    arrays e indices para alumno que se definira en libros

---
+++ Yulmi_Parcial1/src/java/clase/AlumnoController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase;
+
+/**
+ *
+ * @author Yosmeri C. Cruz
+ */
+public class AlumnoController {
+     LibroModel[] tablaAlumno;
+    int indiceArray;
+    
+    public AlumnoController(){
+        this.tablaALumno = new  LibroModel[100];
+        this.indiceArray=0;
+    }
+    
+     public void guardarAlumno( LibroModel cliente){
+        this.tablaALumno[this.indiceArray]=cliente;  
+        this.indiceArray=this.indiceArray+1;
+    }
+    
+    public  LibroModel[] getAlumnos(){
+        return this.tablaALumno;
+    }
+}
